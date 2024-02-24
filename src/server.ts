db0e0b974e7613c4c647cabcf115698a2b9ee132
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-02-24 18:44:59 +0000

    Routes: Created get-all-prompts and upload-video routes

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import { fastify } from "fastify"
+import { getAllPromptsRoute } from "./routes/get-all-prompts"
+import { uploadVideoRoute } from "./routes/upload-video"
 
 const app = fastify()
 
-app.get('/', () => {
-    return 'Hello World'
-})
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
 
 app.listen({
-    port: 3000,
+    port: 3333,
 }).then(() => {
     console.log('HTTP Server Running!')
 })
